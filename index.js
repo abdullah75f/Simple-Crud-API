6811e5e91359b1e1d98697f6b7de85d163112c28
zaz7 <abdullah75farid@gmail.com>
2024-08-10 16:34:01 +0300

    final commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // brain of the backend
 const express  = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 app.listen(3000,()=>{
     console.log('server is listening on port 3000');
