41366079a33aabfd792c4b96154cb9889fa3c6a3
zaz7 <abdullah75farid@gmail.com>
2024-08-10 23:25:32 +0300

    final commit

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
 // brain of the backend
-const express  = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
-const Product = require('./models/product.model.js');
+const Product = require("./models/product.model.js");
 app.use(express.json());
 
-
-app.get('/',(req,res)=>{
-    res.send('Hello from Node API Server');
-
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server");
 });
 
-app.get('/api/products', async (req,res)=>{
-    try{
-        const products = await Product.find({});
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-        res.status(200).json(products);
+app.get('/api/products/:id', async (req,res)=>{
+    try{
+    const product = await Product.findById()
     }
     catch(error){
-        res.status(500).json({"message":error.message});
-        
+        res.status(500).json({'message':error.message});
     }
 })
 
-app.post('/api/products', async (req,res)=>{
-    try{
-       const product =  await Product.create(req.body);
-       res.status(200).json(product);
-    }
-    catch(error){
-        res.status(500).json({message:error.message});
-        
-    }
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
- 
-mongoose.connect('mongodb+srv://abdullah75farid:8EaSXlcvGskghVlJ@backenddb.cwpro.mongodb.net/Node-Api?retryWrites=true&w=majority&appName=BackendDB')
-.then(()=>{
-    console.log('connected to database');
-    app.listen(3000,()=>{
-        console.log('server is listening on port 3000');
+
+mongoose
+  .connect(
+    "mongodb+srv://abdullah75farid:8EaSXlcvGskghVlJ@backenddb.cwpro.mongodb.net/Node-Api?retryWrites=true&w=majority&appName=BackendDB"
+  )
+  .then(() => {
+    console.log("connected to database");
+    app.listen(3000, () => {
+      console.log("server is listening on port 3000");
     });
-})
-.catch(()=>{
-    console.log('connection failed!');    
-})
-
+  })
+  .catch(() => {
+    console.log("connection failed!");
+  });
