e1c1ca98a648deb9641adfc00ecb69ae2508c0f4
zaz7 <abdullah75farid@gmail.com>
2024-08-10 16:39:33 +0300

    final commit

--- index.js
+++ index.js
@@ -3,12 +3,20 @@ const express  = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-app.listen(3000,()=>{
-    console.log('server is listening on port 3000');
-});
 
 app.get('/',(req,res)=>{
     res.send("Hello from Node API Server");
 
+});
+
+mongoose.connect('mongodb+srv://abdullah75farid:8EaSXlcvGskghVlJ@backenddb.cwpro.mongodb.net/Node-Api?retryWrites=true&w=majority&appName=BackendDB')
+.then(()=>{
+    console.log('connected to database');
+    app.listen(3000,()=>{
+        console.log('server is listening on port 3000');
+    });
+})
+.catch(()=>{
+    console.log('connection failed!');    
 })
 
