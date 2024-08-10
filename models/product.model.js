f8ed8b84c0d77e92e2d66cf84a241fa6645267fb
zaz7 <abdullah75farid@gmail.com>
2024-08-10 16:49:58 +0300

    final commit

---
+++ models/product.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = mongoose.Schema(  {
+    name: {
+      type: Sring,
+      required: [true, "Please enter product name!"],
+    },
+
+    quantity: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    image: {
+      type: String,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
