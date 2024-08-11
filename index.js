a8d98958f38dd62f13d3eaa593f4ef5be32b21f6
zaz7 <abdullah75farid@gmail.com>
2024-08-11 13:39:23 +0300

    final commit

--- index.js
+++ index.js
@@ -18,61 +18,9 @@ app.use("/api/products", productRoute);
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server");
 });
-
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-});
-
-//Update a product
-
-app.put("/api/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const product = await Product.findByIdAndUpdate(id, req.body);
-
-    if (!product) {
-      return res.status(404).json({
-        message: "Product not found",
-      });
-    }
-    const updatedProduct = await Product.findById(id);
-
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-});
-
 // delete a product
 
-app.delete("/api/products/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
 
-    const product = await Product.findByIdAndDelete(id);
-
-    if (!product) {
-      res.status(404).json("Product not find");
-    }
-    res.status(200).json({
-      message: "Product deleted sucessfully",
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error.message,
-    });
-  }
-});
 
 mongoose
   .connect(
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../models/product.model");
-const { getProducts } = require("../controllers/product.controller");
+const { getProducts,getProduct,createProduct,updateProduct,deleteProduct } = require("../controllers/product.controller");
 
 router.get("/", getProducts);
 router.get("/:id", getProducts);
-router.post("/");
+router.post("/", createProduct);
+router.put('/:id', updateProduct);
+router.delete('/:id', deleteProduct);
+
 
 module.exports = router;
