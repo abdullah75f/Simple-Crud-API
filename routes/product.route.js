6556521bb125d854421e5327447a61c8b5d6182b
zaz7 <abdullah75farid@gmail.com>
2024-08-11 13:09:11 +0300

    final commit

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const Product = require('../models/product.model');
+const {
+    getProducts
+} = require('../controllers/product.controller');
+
+
+router.get('/',getProducts );
+router.get('/:id',getProducts)
