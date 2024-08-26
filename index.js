3117b95a87d42e3ed17f61c1e9d60354795ebd35
zaz7 <abdullah75farid@gmail.com>
2024-08-27 00:10:17 +0300

    final commit

--- index.js
+++ index.js
@@ -51,4 +51,4 @@ client
       console.log("Your server is listening on port 3000");
     });
   })
-  .catch((err) => console.log("Database connection error:", err));;
+  .catch((err) => console.log("Database connection error:", err));
