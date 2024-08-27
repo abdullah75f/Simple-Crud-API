991e0ccb2d1bd65fd1a659cbec41a038f0946774
zaz7 <abdullah75farid@gmail.com>
2024-08-27 13:08:35 +0300

    final commit

--- Database/Users.js
+++ Database/Users.js
@@ -1,9 +1,14 @@
 const client = require("../index");
 
-// Database insertion Logic
+// Database Registration Logic
 const registerUser = async (user_id, name, password) => {
   const registrationQuery = `INSERT INTO users (user_id,name, password) VALUES ($1,$2,$3)`;
   const register = [user_id, name, password];
-  client.query(registrationQuery, register, (err, res) => {});
-
+  client.query(registrationQuery, register, (err, res) => {
+    if (!err)
+      console.log(`User with Name: ${name} registered succesfully, Thank you!`);
+    else {
+      console.log("Unsuccesfull registration", err.message);
+    }
+  });
 };
