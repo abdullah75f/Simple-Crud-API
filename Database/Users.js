1498aeeed599fe116ba5210ac7243145b9121caa
zaz7 <abdullah75farid@gmail.com>
2024-08-28 23:28:53 +0300

    final commit

--- Database/Users.js
+++ Database/Users.js
@@ -23,23 +23,36 @@ const loginUser = async (current_user) => {
   return new Promise((resolve, reject) => {
     client.query(userQuery, [user_name], (err, res) => {
       if (!err && res.rows.length > 0) {
-        // let userFound = false;
+        let userFound = false;
         for (let i = 0; i < res.rows.length; i++) {
           const dbPassword = res.rows[i].password;
           bcrypt.compare(user_password, dbPassword, (err, result) => {
+            if (err) {
+              return reject(new Error(err.message));
+            }
             if (result) {
               const selected_user = [
                 res.rows[i].name,
                 res.rows[i].password,
                 res.rows[i].user_id,
               ];
-              // userFound = true;
+              userFound = true;
               resolve(selected_user);
-            } else {
-              reject(new Error(`Unsuccesful loggin attempt, ${err.message}`));
             }
+
+            if (i === res.rows.length - 1 && !userFound) {
+              return reject(
+                new Error("Incorrect credential, please try again!")
+              );
+            }
+
+            // else {
+            //   reject(new Error(`Unsuccesful loggin attempt, ${err.message}`));
+            // }
           });
         }
+      } else {
+        return reject(new Error("User not found"));
       }
     });
   });
