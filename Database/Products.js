9140437372813fad8b5c3436a4c680c4cf4f33a7
zaz7 <abdullah75farid@gmail.com>
2024-08-29 00:32:46 +0300

    final commit

--- Database/Products.js
+++ Database/Products.js
@@ -1,6 +1,14 @@
 const {client} = require('../database.config.js');
 const insertProduct= async (name, quantity,price,user_id)=>{
-    const insertProductQuery = `INSERT INTO products(name, quantity,price,user_id) VALUES `
+    const insertProductQuery = `INSERT INTO products(name, quantity,price,user_id) VALUES($1,$2,$3,$4)`
+    const product = [name, quantity,price,user_id];
+
+    return new Promise((resolve,reject)=>{
+        client.query(insertProductQuery,product,(err,res)=>{
+            
+        })
+
+    })
 
 }
 
